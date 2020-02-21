0fde73cc434f2f0106349e42ac21258003ed904d
SHUcream00 <requiemdeciel@gmail.com>
2020-02-21 16:44:42 +0900

    Little improvement

--- pymlb/mlb.py
+++ pymlb/mlb.py
@@ -1,30 +1,39 @@
 import requests
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup as bs
 
 class mlb():
 
-    def get_player_info(*args, **kwargs):
+    def __init__(self):
+        self.test_string = "https://www.mlb.com/dodgers/search?q={0}"
+        self.suggestion = "https://suggest.mlb.com/svc/suggest/v1/min_all/{0}/99999"
+        self.player_page = "https://www.mlb.com/player/{0}"
+
+    def get_player_info(self, *args, **kwargs):
 
         #Get a player's personal information from MLB.com
-        print(kwargs)
-        TEST_STRING = "https://www.mlb.com/dodgers/search?q={0}"
-        SUGGESTION = "https://suggest.mlb.com/svc/suggest/v1/min_all/{0}/99999"
-        PLAYER_PAGE = "https://www.mlb.com/player/{0}"
+        player_string = kwargs['player'].replace(chr(32), '%20')
         try:
-            r = requests.get(SUGGESTION.format(kwargs['player']))
-            for i,j in enumerate(r.json()['suggestions']):
-                print(i, j)
-                if i == 0:
-                    plchldr1 = str(j).split('|')
-                    print(plchldr1)
-                    for k, l  in enumerate(plchldr1):
-                        print(k, len(plchldr1))
-                        if k == len(plchldr1) - 1:
-                            r2 = requests.get(PLAYER_PAGE.format(l))
-                            print(r2.text)
+            r = requests.get(self.suggestion.format(player_string))
+            cands = list(map(lambda x: (x.split('|')[8], x.split('|')[9]), r.json()['suggestions']))
+            print(cands, len(cands), cands[0][1])
+
+            if len(cands) == 1:
+                raw_text = requests.get(self.player_page.format(cands[0][1])).text
+                soup = bs(raw_text, 'lxml')
+                player_img = soup.find(alt=cands[0][0])['src']
+                player_bio = soup.find(class='player-bio')
+                print(player_bio)
+
+
+            else:
+                #ask user to select one player
+                pass
+
             return r
-        except:
-            return 0;
+
+        except Exception as error:
+            print("Error occurred: {0}".format(error))
+            return 0
 
     def get_playet_stats(*args, **kwargs):
         #Get a player's current stats
@@ -44,5 +53,6 @@ class mlb():
         soup = BS(t, "lxml").encode("utf-8")
         print(soup)
 
-#mlb.get_player(player = "trout")
-mlb.test()
+test = mlb()
+test.get_player_info(player = "clayton kershaw")
+#mlb.test()
