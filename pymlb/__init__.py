cad487ea692ccea796d9178976e645d340c45a55
SHUcream00 <requiemdeciel@gmail.com>
2020-03-09 16:03:08 +0900

    minor updates

--- pymlb/__init__.py
+++ pymlb/__init__.py
@@ -11,7 +11,7 @@ Author: Joon-Won Choi https://github.com/SHUcream00
 
 Requirement:
 
-Python 3.5 or higher ==> planning to require 3.6 ++ in near future. not yet 
+Python 3.5 or higher ==> planning to require 3.6 ++ in near future. not yet
 BeautifulSoup4 $ pip install bs4
 lxml $ pip install lxml
 
@@ -20,6 +20,8 @@ Installation:
 Usage:
 
 Disclaimer: I do not own the data you might get by running this script.
+
+It is written in lots of them, check below: 
 
 The accounts, descriptions, data and presentation in the referring page (the "Materials") are proprietary content of MLB Advanced Media, L.P ("MLBAM").
 Only individual, non-commercial, non-bulk use of the Materials is permitted and any other use of the Materials is prohibited without prior written authorization from MLBAM.
--- pymlb/mlb.py
+++ pymlb/mlb.py
@@ -25,7 +25,7 @@ class mlb():
         self.suggestion = "https://suggest.mlb.com/svc/suggest/v1/min_all/{0}/99999"
         self.player_page = "https://www.mlb.com/player/{0}"
         self.pitcher_zone = "https://baseballsavant.mlb.com/player/zone?player_id={0}&player_type=pitcher&season={1}&hfGT=R|" #player id, year
-        self.player_api = "https://statsapi.mlb.com/api/v1/people/{0}"
+        self.player_api = "https://statsapi.mlb.com/api/v1/people/{0}" #player id
         self.player_api2 = "https://statsapi.mlb.com/api/v1/people/{0}?hydrate=currentTeam,team, \
                             stats(type=[yearByYear,yearByYearAdvanced,careerRegularSeason,careerAdvanced,availableStats](team(league)),leagueListId=mlb_hist)"
         self.team_api = "https://statsapi.mlb.com/api/v1/teams?sportId=1&language=en&leagueListId=mlb_hist&activeStatus=B&season={0}" #year
@@ -38,13 +38,13 @@ class mlb():
         player_string = kwargs['player'].replace(chr(32), '%20')
         try:
             r = requests.get(self.suggestion.format(player_string))
-            cands = list(map(lambda x: (x.split('|')[8], x.split('|')[9]), r.json()['suggestions']))
-            print(cands, len(cands), cands[0][1])
+            cands = list(map(lambda x: (x.split('|')[8], x.split('|')[1], x.split('|')[9]), r.json()['suggestions']))
+            print(cands, len(cands), cands[0][2])
 
 
             #Player selection part
             #If there's just one automatically go for it
-            if len(cands) == 1: raw_text = requests.get(self.player_page.format(cands[0][1])).text
+            if len(cands) == 1: raw_text = requests.get(self.player_page.format(cands[0][2])).text
             else:
                 #Need to get user input via input()
                 cands_2 = [i[0] for i in cands]
@@ -56,6 +56,7 @@ class mlb():
             soup = bs(raw_text, 'lxml')
             ##player search part can be changed so that
             player_img = soup.find(alt=cands[0][0])['src']
+            player_info = self.getplayerinfo(cands[0][1])
             #basic info
 
             return r
@@ -64,19 +65,30 @@ class mlb():
             print("Error occurred: {0}".format(error))
             return 0
 
-    def get_playet_stats(*args, **kwargs):
+    def getplayerinfo(self, id):
         #Get a player's current stats
+        if not id: return
+        raw = requests.get(self.player_api.format(id)).json()["people"]
+        '''
+        lots of params
+        '''
+        f
         pass
 
-    def get_game_info(*arg, **kwargs):
+    def getplayerstat(self, id):
+        #Get a player's current stats
+        if not id: return
+        pass
+
+    def getgameinfo(*arg, **kwargs):
         #gameday API?
         pass
 
-    def get_future_schedule(*arg, **kwargs):
+    def getschedule(*arg, **kwargs):
         #schedules page
         pass
 
-    def get_standings(*args, **kwargs):
+    def getstandings(*args, **kwargs):
         #standings page
         pass
 
@@ -88,11 +100,11 @@ class mlb():
 
 if __name__ == "__main__":
     actual = mlb()
-    actual.get_player_info(player = "clayton")
+    actual.get_player_info(player = "clayton kershaw")
 
 
-test = mlb()
-test.get_player_info(player = "clayton")
+#test = mlb()
+#test.get_player_info(player = "clayton kershaw")
 
 
 #mlb.test()
