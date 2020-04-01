a475938b5f0245df01580a4422e299c43f814b74
SHUcream00 <requiemdeciel@gmail.com>
2020-04-02 05:11:27 +0900

    Notes change

--- pymlb/__init__.py
+++ pymlb/__init__.py
@@ -3,7 +3,7 @@
 |    PyMLB      |
 =================
 
-An unofficial MLB.com API for simple player data fetch and a little bit of sabermetrics support.
+An unofficial MLB.com API Wrapper for simple player data fetch and a little bit of sabermetrics support.
 
 Github: https://github.com/SHUcream00/PyMLB
 
@@ -21,7 +21,7 @@ Usage:
 
 Disclaimer: I do not own the data you might get by running this script.
 
-It is written in lots of them, check below: 
+It is written in lots of them, check below:
 
 The accounts, descriptions, data and presentation in the referring page (the "Materials") are proprietary content of MLB Advanced Media, L.P ("MLBAM").
 Only individual, non-commercial, non-bulk use of the Materials is permitted and any other use of the Materials is prohibited without prior written authorization from MLBAM.
